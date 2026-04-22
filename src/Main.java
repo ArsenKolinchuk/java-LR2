d2385c9ddbecbc0e9c0b28aca7e9c7a80c772c82
ArsenKolinchuk <162623014+ArsenKolinchuk@users.noreply.github.com>
2026-04-22 15:27:42 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,42 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int n = 10;
+        double[] x = new double[n];
+        double[] y = new double[n];
+
+        Random random = new Random();
+
+        System.out.println("Генерація значень:");
+
+        // заповнення масиву
+        for (int i = 0; i < n; i++) {
+            x[i] = random.nextDouble();
+            y[i] = Math.cos(x[i]);
+
+            System.out.println("i=" + i +
+                    " x=" + x[i] +
+                    " cos(x)=" + y[i]);
         }
+
+        System.out.println("\nПошук максимального значення:");
+
+        double max = y[0];
+        System.out.println("Початковий max = " + max);
+
+        for (int i = 1; i < n; i++) {
+
+            System.out.println("Порівнюємо max=" + max + " з y[" + i + "]=" + y[i]);
+
+            if (y[i] > max) {
+                max = y[i];
+                System.out.println("Новий max = " + max);
+            }
+        }
+
+        System.out.println("\nРЕЗУЛЬТАТ:");
+        System.out.println("Найбільше значення cos(x): " + max);
     }
 }
